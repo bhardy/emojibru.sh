a04f0b9a9c7021427e8f86b4428fedcf206d973a
Brant Hardy <brant@branthardy.com>
2019-06-03 22:03:36 -0700

    palette v1

--- src/EmojiPicker.js
+++ src/EmojiPicker.js
@@ -4,14 +4,23 @@ import { Picker } from 'emoji-mart'
 import 'emoji-mart/css/emoji-mart.css'
 import css from './styles/EmojiPicker.module.css'
 
+const pickerStyles = {
+  outline: '1px solid var(--color3)',
+  borderColor: 'var(--color3)',
+  borderRadius: 0,
+  boxShadow: '5px 5px 0 0 var(--color1)'
+}
+
 const EmojiPicker = ({ updateTool }) => {
   return (
     <div className={css.picker}>
       <Picker
         native={true}
-        title='Pick your paint…'
-        emoji='point_up_2'
+        title="Pick your paint…"
+        emoji="point_up_2"
         onSelect={(emoji) => updateTool({ paint: emoji.native })}
+        color="var(--color2)"
+        style={pickerStyles}
       />
     </div>
   )
