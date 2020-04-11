3d18f600a508ce074a440f94b5975f018d8907ab
Brant Hardy <brant@branthardy.com>
2020-04-10 17:50:00 -0700

    Switching to canvas step 2

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,89 +3,23 @@ import PropTypes from 'prop-types'
 import { useMouseStatus } from '../hooks/useMouseStatus'
 import css from './Canvas.module.css'
 
-const Cell = ({ paint, row, col, draw }) => {
-  const mouseStatus = useMouseStatus()
+// multiplier -- cell size / offset -- buffer
+const MP = 32
+const OS = 4
 
-  const drawCheck = (row, col) => {
-    if (mouseStatus === 'mousedown') {
-      draw(row, col)
-    }
-  }
-
-  const handleClick = event => {
-    // @note: this stops annoying bugs on touch devices but isn't perfect or thorougly tested
-    event.preventDefault()
-    event.stopPropagation()
-    draw(row, col)
-  }
-
-  return (
-    <span
-      className={css.cell}
-      onMouseDown={() => draw(row, col)}
-      onMouseOver={() => drawCheck(row, col)}
-      onTouchEnd={(event) => handleClick(event)}
-    >
-      {paint}
-    </span>
-  )
-}
-
-Cell.propTypes = {
-  paint: PropTypes.string.isRequired,
-  row: PropTypes.number.isRequired,
-  col: PropTypes.number.isRequired,
-  draw: PropTypes.func.isRequired
-}
-
-const Row = ({ row, rowIndex, draw }) => (
-  <div className={css.row}>
-    {row.map((cell, colIndex) => (
-      <Cell
-        row={rowIndex}
-        col={colIndex}
-        paint={cell}
-        draw={draw}
-        key={`${rowIndex}${colIndex}`}
-      />
-    ))}
-  </div>
-)
-
-Row.propTypes = {
-  row: PropTypes.array.isRequired,
-  rowIndex: PropTypes.number.isRequired,
-  draw: PropTypes.func.isRequired
-}
-
-const drawing = (canvasRef, width, height, grid) => {
+const drawing = (canvasRef, grid) => {
   const canvas = canvasRef.current
   const ctx = canvas.getContext('2d')  
   ctx.font = "26px sans-serif"
 
-  // multiplier -- cell size
-  const mp = 32.5;
-
-  // centres
-  const cx = canvas.width / 2
-  const cy = canvas.height / 2
-
-  // canvas size -- by cells
-  const cw = width * mp
-  const ch = height * mp
-
-  // offests
-  const ox = cx - cw / 2
-  const oy = cy - ch / 2
-
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.save()
-  ctx.translate(ox,oy)
+  ctx.translate(OS,OS)
+  ctx.textBaseline = 'top' 
 
-  // multiplier -- cell size
   grid.forEach((row, yi) => {
     row.forEach((cell, xi) => {
-      ctx.fillText(cell, xi * mp, yi * mp)
+      ctx.fillText(cell, xi * MP, yi * MP)
     })
   })
 
@@ -93,18 +27,42 @@ const drawing = (canvasRef, width, height, grid) => {
   ctx.restore()
 }
 
+const getRelativePosition = (canvasRef, e) => {
+  const canvas = canvasRef.current
+  const rect = canvas.getBoundingClientRect()
+
+  // get grid position
+  const x = e.clientX - rect.left
+  const y = e.clientY - rect.top
+
+  // clicked cell (zero-indexed)
+  const cx = Math.ceil(x / MP) - 1
+  const cy = Math.ceil(y / MP) - 1
+
+  return { cx, cy }
+}
+
+const handleCanvasClick = (canvasRef, e, draw) => {
+  const {cx, cy} = getRelativePosition(canvasRef, e)
+
+  draw(cy, cx)
+}
+
+const handleCanvasDrag = (canvasRef, e, draw, mouseStatus) => {
+  const {cx, cy} = getRelativePosition(canvasRef, e)
+
+  if (mouseStatus === 'mousedown') {
+    draw(cy, cx)
+  }
+}
+
 const Canvas = ({ grid, draw, width, height }) => {
   const canvasRef = useRef(null)
+  const mouseStatus = useMouseStatus()
 
   useEffect(() => {
-    drawing(canvasRef, width, height, grid)
+    drawing(canvasRef, grid)
   }, [width, height, grid])
-
-  function handleCanvasClick(e) {
-    console.log(e.clientX, e.clientY);
-    // const newLocation = { x: e.clientX, y: e.clientY }
-    // setLocations([...locations, newLocation])
-  }
   
   function handleClear() {
     // setLocations([])
@@ -116,22 +74,20 @@ const Canvas = ({ grid, draw, width, height }) => {
 
   return (
     <>
-      {/* <div className={css.canvas}>
-        {grid.map((row, rowIndex) => (
-          <Row row={row} rowIndex={rowIndex} draw={draw} key={rowIndex} />
-          ))}
-      </div> */}
       <div className={css.controls}>
         <button onClick={handleClear}>Clear</button>
         <button onClick={handleUndo}>Undo</button>
       </div>
-      <canvas
-        className={css.realCanvas}
-        ref={canvasRef}
-        width={window.innerWidth}
-        height={window.innerHeight}
-        onClick={handleCanvasClick}
-      />
+      <div className={css.wrapper}>
+        <canvas
+          ref={canvasRef}
+          width={width * MP}
+          height={height * MP}
+          onMouseDown={(e) => handleCanvasClick(canvasRef, e, draw)}
+          onTouchEnd={(e) => handleCanvasClick(canvasRef, e, draw)}
+          onMouseMove={(e) => handleCanvasDrag(canvasRef, e, draw, mouseStatus)}
+        />
+      </div>
     </>
   )
 }
