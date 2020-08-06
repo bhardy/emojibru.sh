37913333d2c4fb848e434c4936539adcaf4304c6
Brant Hardy <brant@branthardy.com>
2020-08-05 19:54:45 -0700

    Adds a basic google analytics setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { RecoilRoot } from 'recoil'
+import ReactGA from 'react-ga';
 import { initializeState } from '../store/store'
 import Header from './Header'
 import Artboard from './Artboard'
 import Toolbar from './Toolbar'
 import Footer from './Footer'
 import css from './App.module.css'
+
+ReactGA.initialize('UA-174636871-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const App = () => {
   return (
