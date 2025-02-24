f0a9b0733b27a3cdc4d348606d3af29fc76c96df
Brant Hardy <brant@branthardy.com>
2025-02-23 17:45:02 -0800

    various updates

--- src/store/store.js
+++ src/store/store.js
@@ -1,55 +1,67 @@
-import { create } from "zustand";
-import { devtools } from "zustand/middleware";
+import { create } from "zustand"
+import { devtools, persist } from "zustand/middleware"
 
 // @note: may want to rename this after we sync to local storage
 const initialPainting = {
   width: 10,
   height: 10,
   grid: [],
-};
+}
 
 const useStore = create(
-  devtools((set) => ({
-    // Painting state
-    painting: initialPainting,
-    setPainting: (update) => {
-      set((state) => {
-        const newPainting = {
-          ...state.painting,
-          ...update,
-        };
-        return { painting: newPainting };
-      });
-    },
-    resetPainting: () => {
-      set({ painting: initialPainting })
-    },
+  devtools(
+    persist((set) => ({
+      // Painting state
+      painting: initialPainting,
+      setPainting: (update) => {
+        set((state) => {
+          const newPainting = {
+            ...state.painting,
+            ...update,
+          }
+          return { painting: newPainting }
+        })
+      },
+      resetPainting: () => {
+        set({ painting: initialPainting })
+      },
 
-    // Tool state
-    tool: {
-      type: "draw",
-      paint: "🧞",
-      alternatePaint: "👾",
-    },
-    setTool: (update) => set((state) => ({ 
+      // Tool state
       tool: {
-        ...state.tool,
-        ...update
-      }
-    })),
+        type: "draw",
+        paint: "🧞",
+        alternatePaint: "👾",
+      },
+      setTool: (update) =>
+        set((state) => ({
+          tool: {
+            ...state.tool,
+            ...update,
+          },
+        })),
 
-   // Palette state
-    palette: ['🙈', '😭', '😕', '🤠', '😌', '🧞', '😂', '🤬', '😝', '💩', '🤢', '😫'],
-    setPalette: (index, paint) => set((state) => {
-      const newPalette = [...state.palette];
-      newPalette[index] = paint;
-      return { palette: newPalette };
-    }),
+      // Palette state
+      palette: ["🙈", "😭", "😕", "🤠", "😌", "🧞", "😂", "🤬", "😝", "💩", "🤢", "😫"],
+      setPalette: (index, paint) =>
+        set((state) => {
+          const newPalette = [...state.palette]
+          newPalette[index] = paint
+          return { palette: newPalette }
+        }),
 
-    // Allow shortcuts state
-    allowShortcuts: true,
-    setAllowShortcuts: (allowShortcuts) => set({ allowShortcuts }),
-  })),
-);
+      // Allow shortcuts state
+      allowShortcuts: true,
+      setAllowShortcuts: (allowShortcuts) => set({ allowShortcuts }),
+    }), {
+      // Cycles to local storage
+      name: "emoji-brush",
+      partialize: (state) => ({
+        painting: state.painting,
+        tool: state.tool,
+        palette: state.palette,
+      })
+    })
+  )
+)
 
-export default useStore;
+export default useStore
