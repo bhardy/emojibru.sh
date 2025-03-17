aa6164a670b306081b77c4617bba7a86632115a5
Brant Hardy <brant@branthardy.com>
2025-03-16 17:35:13 -0700

    updates favicons & manifest

---
+++ public/sw.ts
@@ -0,0 +1,2 @@
+// https://nextjs.org/docs/app/building-your-application/configuring/progressive-web-apps#5-creating-a-service-worker
+// this file only exists to override the old service worker from create-react-app
