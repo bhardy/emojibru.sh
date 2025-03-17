d84b905b03723557805bd4e75683cac3199c3b52
Brant Hardy <brant@branthardy.com>
2025-03-16 18:01:10 -0700

    attempt to clear service worker

--- public/sw.ts
+++ public/sw.ts
@@ -1,2 +1,10 @@
 // https://nextjs.org/docs/app/building-your-application/configuring/progressive-web-apps#5-creating-a-service-worker
 // this file only exists to override the old service worker from create-react-app
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister()
+    }
+  })
+}
