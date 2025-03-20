7c71a3a53e546bd4c65718d29d2ee69b3c308f48
Brant Hardy <brant@branthardy.com>
2025-03-20 13:54:54 -0700

    updates meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,26 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'EmojiBrush',
-  description: 'Make art for your friends (and enemies) with Emojis',
+  title: 'EmojiBrush - Create Emoji Art',
+  description: 'Create beautiful Emoji art for your friends and enemies',
   metadataBase: new URL('https://emojibru.sh'), // @todo: add env vars to handle this
+  keywords: [
+    'emoji art',
+    'emoji creator',
+    'emoji drawing',
+    'online emoji art tool',
+    'creative emoji',
+    'digital emoji art',
+    'emoji canvas',
+  ],
+  creator: 'Brant Hardy',
+  alternates: {
+    canonical: 'https://emojibru.sh',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 }
 
 export default function RootLayout({
